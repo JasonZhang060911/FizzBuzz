443bbf43b0687f0fc13a3cae528b731f0bbef4fd
JasonZhang060911 <jasonn.zhang@mail.utoronto.ca>
2025-09-08 17:01:11 -0400

    add public for multiples method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,10 +2,22 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        System.out.println(multiples(10,3,5));
+        // Calls the overloaded version (n=1000, a=3, b=5)
+        int result1 = multiples();
+
+        // Calls the original version
+        int result2 = multiples(20, 3, 5);
+
+        System.out.println("Default result: " + result1);
+        System.out.println("Custom result: " + result2);
     }
 
-    static int multiples(int n, int a, int b) {
+    public static int multiples() {
+        // Calls the other multiples method with default values
+        return multiples(1000, 3, 5);
+    }
+
+    public static int multiples(int n, int a, int b) {
         int counter = 0;
         for(int i=1;i<n;i++) {
             if (i % a == 0 || i % b == 0) {
